5ce64818a713461a6257e927d77fd6f7da609950
Matt Garcia <mattg7061@gmail.com>
2021-06-12 20:26:11 -0700

    Added AI to enemy

--- Assets/RedAI.cs
+++ Assets/RedAI.cs
@@ -4,20 +4,48 @@ using UnityEngine;
 
 public class RedAI : MonoBehaviour
 {
-    // Start is called before the first frame update
-    enum mood
-    { idle, chase, attack };
+    private Rigidbody2D rb;
+    public enum MoodEnum { idle, chase, attack };
+    public MoodEnum mood;
+    public float SightRange;
+    public int ResetTime;
+    public int speed;
+    private Vector2 dir;
+    public GameObject ReturnTo;
+    public GameObject Player;
 
 
     void Start()
     {
-        mood mymood;
-        mymood = mood.idle;
+        mood = MoodEnum.idle;
+        rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
     void Update()
     {
+        var a = new Vector2(transform.position.x, transform.position.y);
+        var b = new Vector2(Player.transform.position.x, Player.transform.position.y);
+        if (Vector2.Distance(a, b) > SightRange * 1.5)
+        {
+            mood = MoodEnum.idle;
+        }
+
+        if (Vector2.Distance(a, b) < SightRange)
+        {
+            mood = MoodEnum.chase;
+        }
+        if (mood == MoodEnum.chase)
+        {
+            dir = Player.transform.position - transform.position;
+            dir = dir.normalized;
+            rb.AddForce(dir * speed);
+        }
+        if (mood == MoodEnum.idle)
+        {
+            dir = ReturnTo.transform.position - transform.position;
+            dir = dir.normalized;
+            rb.AddForce(dir * speed);
+        }
 
     }
 }
